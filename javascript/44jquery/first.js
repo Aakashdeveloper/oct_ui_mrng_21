342eebb030252c3dad4e0ab05c3ce9744e962915
aakashdeveloper <ahanda205@gmail.com>
2021-11-26 08:47:25 +0530

    jquery

---
+++ javascript/44jquery/first.js
@@ -0,0 +1,14 @@
+$('h1')
+m.fn.init [h1, prevObject: m.fn.init(1), context: document, selector: 'h1']
+$('h1')[0]
+<h1>​I'm AAKASH HANDA​</h1>​
+$('h1')[0].innerText
+"I'm AAKASH HANDA"
+$('.scroll')
+m.fn.init(5) [a.scroll, a.scroll, a.scroll, a.scroll, a.scroll, prevObject: m.fn.init(1), context: document, selector: '.scroll']
+$('.scroll')[3]
+             
+<a href=​"#education" class=​"scroll">​Education​</a>​
+$('.scroll')[3].innerText="Edu"
+             
+'Edu'
